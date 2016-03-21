f2aed1cac40278d420bb61a9ab0f7fe6cf0af293
Shane Kumpf <sakserv@sakserv.org>
2016-03-21 16:59:21 -0600

    change restart order so the JHS can create files in hdfs

--- roles/common/files/restart-hadoop-master-daemons.sh
+++ roles/common/files/restart-hadoop-master-daemons.sh
@@ -6,8 +6,9 @@ ${HADOOP_HOME}/sbin/yarn-daemon.sh stop resourcemanager
 ${HADOOP_HOME}/sbin/yarn-daemon.sh start resourcemanager
 
 ${HADOOP_HOME}/sbin/mr-jobhistory-daemon.sh stop historyserver
-${HADOOP_HOME}/sbin/mr-jobhistory-daemon.sh start historyserver
 
 ${HADOOP_HOME}/sbin/hadoop-daemon.sh stop namenode
 ${HADOOP_HOME}/bin/hadoop namenode -format -nonInteractive || true
 ${HADOOP_HOME}/sbin/hadoop-daemon.sh start namenode
+
+${HADOOP_HOME}/sbin/mr-jobhistory-daemon.sh start historyserver
