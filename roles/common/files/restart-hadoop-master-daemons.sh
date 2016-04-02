6d16ad6cfd847c9d7e36dd4f352e6f24fc0a27f9
Shane Kumpf <sakserv@sakserv.org>
2016-04-02 06:21:35 -0600

    fix container-executor perms, bootstrap hdfs dirs

--- roles/common/files/restart-hadoop-master-daemons.sh
+++ roles/common/files/restart-hadoop-master-daemons.sh
@@ -11,4 +11,7 @@ su - hdfs -c "${HADOOP_HOME}/sbin/hadoop-daemon.sh stop namenode"
 su - hdfs -c "${HADOOP_HOME}/bin/hadoop namenode -format -nonInteractive || true"
 su - hdfs -c "${HADOOP_HOME}/sbin/hadoop-daemon.sh start namenode"
 
+su - hdfs -c "${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /tmp/hadoop-yarn"
+su - hdfs -c "${HADOOP_HOME}/bin/hdfs dfs -chown yarn:hadoop /tmp/hadoop-yarn"
+
 su - yarn -c "${HADOOP_HOME}/sbin/mr-jobhistory-daemon.sh start historyserver"
