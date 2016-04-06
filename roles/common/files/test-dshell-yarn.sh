8601a0e5c0bc8a0d65749162a6bbe1c68e1b5f85
Shane Kumpf <sakserv@sakserv.org>
2016-04-06 16:00:22 -0600

    added non docker dshell test script

---
+++ roles/common/files/test-dshell-yarn.sh
@@ -0,0 +1,4 @@
+source env.sh
+export DSHELL_JAR=./share/hadoop/yarn/hadoop-yarn-applications-distributedshell-*.jar
+
+yarn jar $DSHELL_JAR -shell_command "exit 1" -jar $DSHELL_JAR -num_containers 1
