b278c8fda872f72b1418c70b2ea749b3399d7570
Shane Kumpf <sakserv@sakserv.org>
2017-04-21 08:16:00 -0600

    adjust debug delay and test job run times

--- roles/common/files/test-dshell-yarn.sh
+++ roles/common/files/test-dshell-yarn.sh
@@ -1,4 +1,4 @@
 source env.sh
 export DSHELL_JAR=./share/hadoop/yarn/hadoop-yarn-applications-distributedshell-*.jar
 
-yarn jar $DSHELL_JAR -shell_command "sleep 120" -jar $DSHELL_JAR -num_containers 1
+yarn jar $DSHELL_JAR -shell_command "sleep 60" -jar $DSHELL_JAR -num_containers 1
