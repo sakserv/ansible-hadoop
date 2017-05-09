ab1a44ddd9204800e3b17a7c613a8f0960a1b89a
Shane Kumpf <sakserv@sakserv.org>
2017-05-09 08:41:36 -0600

    limit to single mapper for pi test

--- roles/common/files/test-pi-docker-yarn.sh
+++ roles/common/files/test-pi-docker-yarn.sh
@@ -1,4 +1,4 @@
 source env.sh
 export YARN_EXAMPLES_JAR=./share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar
 CNTR_ID="sakserv/docker-hadoop-build:hadoop-3.0.0-alpha2"
-bin/yarn jar $YARN_EXAMPLES_JAR pi -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dyarn.app.mapreduce.am.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" 4 10000
+bin/yarn jar $YARN_EXAMPLES_JAR pi -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dyarn.app.mapreduce.am.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" 1 40000
--- roles/common/files/test-pi-yarn.sh
+++ roles/common/files/test-pi-yarn.sh
@@ -1,3 +1,3 @@
 source env.sh
 export YARN_EXAMPLES_JAR=./share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar
-bin/yarn jar $YARN_EXAMPLES_JAR pi 4 10000
+bin/yarn jar $YARN_EXAMPLES_JAR pi 1 40000
