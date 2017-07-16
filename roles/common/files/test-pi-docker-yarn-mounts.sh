d92b103cfc278bf5a3996ea2cc2dec59225aaab3
Shane Kumpf <sakserv@sakserv.org>
2017-07-16 07:13:45 -0600

    clean up pi docker mounts scripts

--- roles/common/files/test-pi-docker-yarn-mounts.sh
+++ roles/common/files/test-pi-docker-yarn-mounts.sh
@@ -1,4 +1,17 @@
 source env.sh
+
+# JAR
 export YARN_EXAMPLES_JAR=./share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar
+
+# ENV Vars
 CNTR_ID="sakserv/docker-hadoop-build:hadoop-3.0.0-alpha2"
-bin/yarn jar $YARN_EXAMPLES_JAR pi -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID,YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS='/tmp:/newtmp:rw,/etc/passwd:/tmp/passwd:ro'" -Dyarn.app.mapreduce.am.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID,YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS='/tmp:/newtmp:rw,/etc/passwd:/tmp/passwd:ro'" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID,YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS='/tmp:/newtmp:rw,/etc/passwd:/tmp/passwd:ro'" 1 40000
+MAP_ENV="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID,YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS='/tmp:/newtmp:rw,/etc/passwd:/tmp/passwd:ro'"
+REDUCE_ENV="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID,YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS='/tmp:/newtmp:rw,/etc/passwd:/tmp/passwd:ro'"
+AM_ENV="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID,YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS='/tmp:/newtmp:rw,/etc/passwd:/tmp/passwd:ro'"
+
+# Run PI job
+bin/yarn jar $YARN_EXAMPLES_JAR pi \
+	-Dmapreduce.map.env="$MAP_ENV" \
+	-Dyarn.app.mapreduce.am.env="$AM_ENV" \
+	-Dmapreduce.reduce.env="$REDUCE_ENV" \
+	1 40000
