280aea543bc8547d0da3c20af98b7d638be3d5bd
Shane Kumpf <sakserv@sakserv.org>
2017-11-27 12:52:31 -0700

    add script to restart the rm

---
+++ roles/common/files/restart-resourcemanager.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ${HADOOP_HOME}/env.sh
+
+su - yarn -c "${HADOOP_HOME}/sbin/yarn-daemon.sh stop resourcemanager"
+su - yarn -c "${HADOOP_HOME}/sbin/yarn-daemon.sh start resourcemanager"
