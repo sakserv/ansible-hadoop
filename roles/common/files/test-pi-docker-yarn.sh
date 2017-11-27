98e39bc8c2f0b0f9a8e0e908dbb0c26078d49a5a
Shane Kumpf <sakserv@sakserv.org>
2017-11-27 14:48:10 -0700

    default to only running mr tasks in docker containers

--- roles/common/files/test-pi-docker-yarn.sh
+++ roles/common/files/test-pi-docker-yarn.sh
@@ -1,4 +1,4 @@
 source env.sh
 export YARN_EXAMPLES_JAR=./share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar
 CNTR_ID="hadoop-build:latest"
-bin/yarn jar $YARN_EXAMPLES_JAR pi -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dyarn.app.mapreduce.am.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" 1 40000
+bin/yarn jar $YARN_EXAMPLES_JAR pi -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" 1 40000
--- roles/common/files/test-sleep-docker-yarn.sh
+++ roles/common/files/test-sleep-docker-yarn.sh
@@ -1,4 +1,4 @@
 source env.sh
 export YARN_EXAMPLES_JAR=./share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-*-tests.jar
 CNTR_ID="hadoop-build:latest"
-bin/yarn jar $YARN_EXAMPLES_JAR sleep -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dyarn.app.mapreduce.am.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" sleep -m 1 -r 0 -mt 60000
+bin/yarn jar $YARN_EXAMPLES_JAR sleep -Dmapreduce.map.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" -Dmapreduce.reduce.env="YARN_CONTAINER_RUNTIME_TYPE=docker,YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$CNTR_ID" sleep -m 1 -r 0 -mt 60000
